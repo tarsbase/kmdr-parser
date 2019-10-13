b99a79a22ee4c1b7f02ff446a500f74aa58063d6
Eddie Ramirez <djedi.r@gmail.com>
2019-10-13 09:26:26 -0700

    Export named interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import { OptionSchema, ProgramSchema, SubcommandSchema } from "./interfaces";
 import { Option } from "./option";
 import { Parser } from "./parser";
 import { Program } from "./program";
 import Schema from "./schema";
 import { Subcommand } from "./subcommand";
 
-export { Parser, Program, Option, Subcommand };
+export {
+  Parser,
+  Program,
+  Option,
+  Subcommand,
+  OptionSchema,
+  ProgramSchema,
+  SubcommandSchema
+};
+
 export default Schema;
